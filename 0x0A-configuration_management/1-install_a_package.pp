014c37143feceb6a50d065afb7a2e6397f1f3ceb
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-11-24 23:55:09 +0200

    python3-pip first

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Installs flask, version 2.1.0.
+#!/usr/bin/pup
+# Install a specific version of flask 2.1.0
+package { 'python3-pip':
+  ensure => installed,
+}
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
