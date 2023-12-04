08ca47dfcffc31b23da4b1d012ecc8b538f2510e
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-12-04 16:50:12 +0200

    shellcheck error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,25 @@
 # Automating the nginx installing process and adding custom header.
 
-package { 'nginx':
-  ensure => installed,
+exec { 'update':
+  provider => shell,
+  command => 'sudo apt-get -y update',
+  before => Exec['install Nginx'],
 }
 
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Package['nginx'],
+exec { 'install Nginx':
+  provider => shell,
+  command => 'sudo apt-get -y install nginx',
+  before => Exec['add_header'],
 }
 
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => present,
-  content => 'Hello World!',
-  require => Package['nginx'],
+exec { 'add_header':
+  provider => shell,
+  environment => ["HOST=${hostname}"],
+  command => 'sudo sh -c \'echo "add_header X-Served-By $hostname;" | tee /etc/nginx/conf.d/custom_header.conf\'',
+  before => Exec['restart Nginx'],
 }
 
-file { '/etc/nginx/conf.d/custom_header.conf':
-  ensure  => present,
-  content => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-exec { 'allow_http':
-  command => 'ufw allow "Nginx HTTP"',
-  path    => ['/bin', '/usr/bin'],
-  unless  => 'ufw status | grep "Nginx HTTP"',
-  require => Package['nginx'],
+exec { 'restart Nginx':
+  provider => shell,
+  command => 'sudo service nginx restart',
 }
