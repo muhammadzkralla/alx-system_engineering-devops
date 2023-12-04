b7f49f5208963084446d2fd924fc3e9b0ca4cac6
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-12-04 16:56:22 +0200

    sed -i used instead of echo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,24 +2,24 @@
 
 exec {'update':
   provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+  command => 'sudo apt-get -y update',
+  before => Exec['install'],
 }
 
-exec {'install Nginx':
+exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
+  command => 'sudo apt-get -y install nginx',
+  before => Exec['header'],
 }
 
-exec { 'add_header':
-  provider    => shell,
+exec { 'header':
+  provider => shell,
   environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
+  command => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before => Exec['restart'],
 }
 
-exec { 'restart Nginx':
+exec { 'restart':
   provider => shell,
-  command  => 'sudo service nginx restart',
+  command => 'sudo service nginx restart',
 }
