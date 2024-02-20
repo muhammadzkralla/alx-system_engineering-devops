c0f5305d864c3319910b474cfbd6f305f9c13517
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-20 02:43:52 +0200

    pycode

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,22 +5,26 @@ returns information about his/her TODO list progress.
 """
 
 import requests
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: {} employee_id".format(argv[0]))
-        exit(1)
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        sys.exit(1)
 
-    employee_id = argv[1]
+    employee_id = sys.argv[1]
 
     # Fetching employee info
-    user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                             .format(employee_id)).json()
+    user_info = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    ).json()
 
     # Fetching TODO list
-    todo_list = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                             .format(employee_id)).json()
+    todo_list = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            employee_id)
+    ).json()
 
     total_tasks = len(todo_list)
     done_tasks = [task for task in todo_list if task.get('completed')]
